592b96f2722c8a43060c6642e3990e70045dd31d
Laurizete <lorealves2000@gmail.com>
2026-05-04 16:57:53 -0300

    primeiro commit

---
+++ teste.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!");
+//npm init -y //cria o package.json com as configurações padrão
+//npm install express //instala o express e adiciona a dependência no package.json  
+  
