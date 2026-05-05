5e4c06dabaeac67687531784f6bd1b8a372ea26c
Laurizete <lorealves2000@gmail.com>
2026-05-05 15:18:53 -0300

    atualização src

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Hello, World!");
+console.log("Ola,Lore!"); 
+// node --watch src/index.js    serve para rodar o código e ficar observando as mudanças, ou seja, toda
+//  vez que salvar o arquivo, ele irá rodar novamente.
+// para parar o processo, basta usar Ctrl + C no terminal.
